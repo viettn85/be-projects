2d6d7892552967e25f8604bc055f97f9b0804ec7
Viet Tran <viet_tran@singaporeair.com.sg>
2020-07-02 07:18:57 +0800

    Update shop app

--- nodejs/shop/api/routes/orders.js
+++ nodejs/shop/api/routes/orders.js
@@ -14,8 +14,13 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  };
   res.status(200).json({
     message: 'Post OK',
+    order: order,
   });
 });
 
--- nodejs/shop/api/routes/products.js
+++ nodejs/shop/api/routes/products.js
@@ -1,22 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Get OK',
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log('Found ', docs);
+      res.status(200).json(docs);
+    })
+    .catch((error) => {
+      console.log('Error', error.message);
+      res.status(500).json({
+        error: error.message,
+      });
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'ID ' + req.params.productId,
-  });
+  Product.findById(req.params.productId)
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        console.log('Found ', doc);
+        res.status(200).json(doc);
+      } else {
+        console.log('Not Found');
+        res.status(404).json({
+          message: 'Not existing ID',
+        });
+      }
+    })
+    .catch((error) => {
+      console.log('Error', error.message);
+      res.status(500).json({
+        error: error.message,
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Post OK',
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
   });
+  product
+    .save()
+    .then((result) => {
+      console.log('Saved new product');
+      res.status(200).json({
+        message: 'Saved new product',
+        product: product,
+      });
+    })
+    .catch((err) => {
+      console.log('Error to save new product');
+      res.status(500).json({
+        error: error.message,
+      });
+    });
+});
+
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  const updateObject = {};
+  for (const ops in req.body) {
+    updateObject[ops] = req.body[ops];
+  }
+  Product.update({ _id: id }, { $set: updateObject })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: error.message,
+      });
+    });
+});
+
+router.delete('/', (req, res, next) => {
+  Product.remove({ _id: req.body.id })
+    .exec()
+    .then((result) => {
+      console.log('Deleted a product with ID ', req.body.id);
+      res.status(200).json({
+        message: 'Deleted',
+        id: req.body.id,
+      });
+    })
+    .catch((err) => {
+      console.log('Error to delete a product');
+      res.status(500).json({
+        error: error.message,
+      });
+    });
 });
 
 module.exports = router;
